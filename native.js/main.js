5c2a7ad8f01f9cf4bbbb272bafe284ee2507fb1a
arinasp <johndoe@example.com>
2025-07-14 16:12:46 +0300

    1

--- native.js/main.js
+++ native.js/main.js
@@ -1,6 +1,59 @@
+const { handleError } = require("vue");
+
 console.log("Скрипт запущен");
 alert("Скрипт запущен");
+const habit={
+    name:'',
+    description:'',
+    frequency:0,
+    count:0,
+    sum:0,
 
+};
+
+const nameMeta={
+    selector:document.querySelector('#habit-name'),
+    handler:(ev)=>{
+        habit.name=ev.target.value;
+        console.log(name);
+    }
+}
+
+const descriptionMeta = {
+    selector: document.querySelector('#description'),
+    handler: (ev) => {
+        habit.description = ev.target.value;
+        console.log('habit.description:', habit.description);
+    }
+};
+
+const frequencyMeta = {
+    selector: document.querySelector('#frequency'),
+    handler: (ev) => {
+        const value = ev.target.value;
+        habit.frequency = value ? parseInt(value) : 0;
+        console.log('habit.frequency:', habit.frequency);
+        setSum();
+    }
+};
+
+const countMeta = {
+    selector: document.querySelector('#count'),
+    handler: (ev) => {
+        const value = ev.target.value;
+        habit.count = value ? parseInt(value) : 0;
+        console.log('habit.count:', habit.count);
+        setSum();
+    }
+}
+const metaData=[nameMeta];
+for (const {selector, handler} of metaData){
+    if(selector.tagName.toLowerCase()== 'select'){
+        selector.addEventListener('change', handler)
+    }
+    else
+        selector.addEventListener('input', handler)
+}
 let name = '';
 let description = '';
 let frequency = 0;
@@ -41,12 +94,7 @@ function setSum() {
     sum = frequency * count;
     sumElement.textContent = sum;
     console.log("Сумма обновлена:", sum);
+
 }
 
-const formElement = document.querySelector('form');
-formElement.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    description = descriptionInput.value;
-    console.log(name, description, frequency, count, sum);
-    alert(`Навык сохранён!\nИмя: ${name}\nОписание: ${description}\nЧастота: ${frequency}\nКоличество: ${count}\nИтого: ${sum}`);
-});
+const formElement=
